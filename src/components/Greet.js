ecefdd4a10b2073e7870a148b5af25fc8469d57a
AyeshaNoor25 <kiani6824@gmail.com>
2024-09-16 09:27:21 +0500

    Change in greet

---
+++ src/components/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// function Greet(){
+//     return <h1> This is first line</h1>
+// }
+
+const Greet= (props) => {return <h1>Hello! {props.name} </h1>}
+export default Greet;
